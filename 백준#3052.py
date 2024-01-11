f2e46da3e0a763d08a73e092b4e90a8ab7938218
minhyeok <rapaell97@naver.com>
2024-01-11 17:55:44 +0900

    dd

---
+++ 백준#3052.py
@@ -0,0 +1,17 @@
+alist=list()
+for i in range(10):
+    a=int(input())
+    alist.append(a)
+rlist=[]
+for k in alist:
+    remain = k%42
+    rlist.append(remain)
+
+rlist1=set(rlist)
+rlist2=list(rlist1)
+
+for j in range(10):
+    rlist2.count(j)
+    
+
+
