e2774a1ece5ad4ca1a068a6364916c113a0e5eee
minhyeok <rapaell97@naver.com>
2024-01-17 17:52:41 +0900

    d

--- 백준#.py
+++ 백준#.py
@@ -4,16 +4,18 @@ for i in range(test):
     word_list = list(map(str,input().split()))
 
 cnt=0
+
 for k in word_list:
     len_word = len(k)
-    for m in range(len_word):
+    for m in range(len_word-1):
         if k[m] != k[m+1]:
             for n in range(m+2,len_word):
                 if k[n] == k[m]:
                     break
                 else:
                     cnt+=1
-        break
+        else:
+            ??
 
 
 print(cnt)
