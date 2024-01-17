103ec3076c31515c24ae402c2ab0df509a554b87
minhyeok <rapaell97@naver.com>
2024-01-17 17:11:13 +0900

    실습개어려웠음

--- 백준#.py
+++ 백준#.py
@@ -1,1 +1,19 @@
-ㅇㅇㅇ
+test = int(input())
+
+for i in range(test):
+    word_list = list(map(str,input().split()))
+
+cnt=0
+for k in word_list:
+    len_word = len(k)
+    for m in range(len_word):
+        if k[m] != k[m+1]:
+            for n in range(m+2,len_word):
+                if k[n] == k[m]:
+                    break
+                else:
+                    cnt+=1
+        break
+
+
+print(cnt)
