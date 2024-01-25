988ecbcda6a08d4715f29b4d910752251caf6263
minhyeok <rapaell97@naver.com>
2024-01-26 01:26:51 +0900

    d

--- 연습.py
+++ 연습.py
@@ -1,18 +1,7 @@
-lst=[[0 for _ in range(3)]for _ in range(2)]
-lst2=list(map(int,input().split()))
-
-for i in range(len(lst)):
-    for k in range(len(lst[i])):
-        lst[i][k]=lst2.pop()
-
-lst3=[0 for _ in range(6)]
-
+word = input()
+print(len(word))
 cnt=0
-for i in range(len(lst)):
-    for k in range(len(lst[i])):
-        lst3[cnt]=lst[i][k]
+for i in word:
+    if i == word[-1]:
         cnt+=1
-
-lst3[0],lst3[5]=lst3[5],lst3[0]
-
-print(*lst3)
+print(cnt)
