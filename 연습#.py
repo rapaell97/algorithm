66fa74cf3b89a2632466c52ab3d3b4f10131b758
minhyeok <rapaell97@naver.com>
2024-02-01 22:23:01 +0900

    d

--- 연습#.py
+++ 연습#.py
@@ -1,22 +1,5 @@
-lst=[[0 for _ in range(4)] for _ in range(4)]
-x = 1
-for i in range(len(lst)):
-    for k in range(len(lst[i])):
-        lst[i][k] = x
-        x+=1
+test = int(input())
+lst = [int(input()) for _ in range(test)]
 
-lst2 = [[0 for _ in range(4)] for _ in range(4)]
-num = list(map(int,input().split()))
-
-for j in range(len(num)):
-    for i in range(len(lst)):
-        for k in range(len(lst[i])):
-            if num[j] == lst[i][k]:
-                lst2[i][k] = j+1
-
-for i in lst2:
-    for k in i:
-        print(k,end=' ')
-    print()
 
 
