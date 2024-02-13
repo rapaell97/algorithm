edb71267e8f0c3d5a1486e1547bf5f2f57e5ccea
minhyeok <rapaell97@naver.com>
2024-02-13 11:09:53 +0900

    d

---
+++ 연습.py
@@ -0,0 +1,18 @@
+N = int(input())
+lst = list()
+for i in range(N):
+    lst.append(int(input()))
+lst.sort()
+stack = [0]
+i = 0
+while i < N:
+    temp = lst[i]
+    if teno > stack[-1]:
+        for i in range(lst[i]-stack[-1]):
+            print('+')
+            stack.append(i)
+    if lst[i] == stack[-1]:
+        print('-')
+        stack.pop()
+
+    elif lst[i] < stack[-1]:
