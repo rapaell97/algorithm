b825b3d422ac881efbcaebc6a2359a2b4e5f30c7
minhyeok <rapaell97@naver.com>
2024-02-28 19:22:48 +0900

    240228

--- 3.BFS/BOJ#2206_벽 부수고 이동하기(retry).py
+++ 3.BFS/BOJ#2206_벽 부수고 이동하기(retry).py
@@ -1,20 +1,19 @@
 from collections import deque
-import copy
 import sys
-sys.stdin = open ('my_input.txt', 'r')
 input = sys.stdin.readline
 
 def bfs(i, k, chance):
     global ans
     temp = 1
-    V = [[0] * M for _ in range(N)]
+    V = [[[0, 0] for _ in range(M)] for _ in range(N)]
     queue = deque()
-    queue.append((i, k, chance, V))
-    V[i][k] = 1
+    queue.append((i, k, chance))
+    V[i][k][1] = 1
+    V[i][k][0] = 0
     while queue:
 
         for _ in range(len(queue)):
-            i, k, chance, V = queue.popleft()
+            i, k, chance = queue.popleft()
 
             if i == N - 1 and k == M - 1:
                 if temp < ans:
@@ -25,18 +24,14 @@ def bfs(i, k, chance):
                 ni = i + di[j]
                 nk = k + dk[j]
 
-                if 0<= ni <N and 0<= nk <M and V[ni][nk] == 0:
-                    if lst[ni][nk] == '0':
-                        V[ni][nk] = 1
-
-                        V2 = copy.deepcopy(V)
-                        queue.append((ni, nk, chance, V2))
+                if 0<= ni <N and 0<= nk <M :
+                    if lst[ni][nk] == '0' and V[ni][nk][chance] == 0:
+                        V[ni][nk][chance] = 1
+                        queue.append((ni, nk, chance))
 
                     if chance == 1 and lst[ni][nk] == '1':
-                        V[ni][nk] = 1
-
-                        V2 = copy.deepcopy(V)
-                        queue.append((ni, nk, chance - 1, V2))
+                        V[ni][nk][0] = 1
+                        queue.append((ni, nk, chance - 1))
         temp += 1
 
     else:
---
+++ BOJ###.py
@@ -0,0 +1,3 @@
+visited = [[[0] * 2 for _ in range(4)] for _ in range(6)]
+
+print(visited)
