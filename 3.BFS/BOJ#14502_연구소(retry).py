110365fd031e9255ad1ad01f09d83b558bad480b
minhyeok <rapaell97@naver.com>
2024-03-12 00:46:24 +0900

    240312

--- 3.BFS/BOJ#14502_연구소(retry).py
+++ 3.BFS/BOJ#14502_연구소(retry).py
@@ -1,35 +1,16 @@
 import sys
-sys.stdin = open ('../my_input.txt', 'r')
-import copy
 from collections import deque
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-lst = [list(map(int, input().split())) for _ in range(N)]
-di = [-1, 0, 1, 0]
-dk = [0, 1, 0, -1]
-
-def make_wall(x):
-    if x == 3:
-        bfs()
-        return
-
-    for i in range(N):
-        for k in range(M):
-            if lst[i][k] == 0:
-                lst[i][k] = 1
-                make_wall(x + 1)
-                lst[i][k] = 0
-
-def bfs():
-    global ans
-    sample_lst = copy.deepcopy(lst)
+def bfs(arr):
+    V = [[0] * M for _ in range(N)]
+    for i, j in arr:
+        lst[i][j] = 1
+    cnt = S - 3
     queue = deque()
-
-    for i in range(N):
-        for k in range(M):
-            if sample_lst[i][k] == 2:
-                queue.append((i, k))
+    for vi, vj in virus:
+        queue.append((vi, vj))
+        V[vi][vj] = 1
 
     while queue:
         p, q = queue.popleft()
@@ -38,18 +19,37 @@ def bfs():
             np = p + di[j]
             nq = q + dk[j]
 
-            if 0<= np <N and 0<= nq <M and sample_lst[np][nq] == 0:
-                sample_lst[np][nq] = 2
+            if 0<= np <N and 0<= nq <M and lst[np][nq] == 0 and V[np][nq] == 0:
                 queue.append((np, nq))
+                V[np][nq] = 1
+                cnt -= 1
 
-    temp = 0
-    for i in range(N):
-        for k in range(M):
-            if sample_lst[i][k] == 0:
-                temp += 1
+    for i, j in arr:
+        lst[i][j] = 0
 
-    ans = max(temp, ans)
+    return cnt
+
+
+N, M = map(int, input().split())
+lst = [list(map(int, input().split())) for _ in range(N)]
+di = [-1, 0, 1, 0]
+dk = [0, 1, 0, -1]
+
+wall_lst = []
+virus = []
+for i in range(N):
+    for k in range(M):
+        if lst[i][k] == 0:
+            wall_lst.append((i, k))
+        elif lst[i][k] == 2:
+            virus.append((i, k))
+
+S = len(wall_lst)
 
 ans = 0
-make_wall(0)
+for i in range(S-2):
+    for j in range(i+1, S-1):
+        for k in range(j+1, S):
+            ans = max(ans, bfs([wall_lst[i], wall_lst[j], wall_lst[k]]))
+
 print(ans)
