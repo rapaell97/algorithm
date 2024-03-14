944bbe5901baeb26cf172668d6e934d9e6890b22
minhyeok <rapaell97@naver.com>
2024-03-14 17:58:55 +0900

    240314

---
+++ 2.DFS/BOJ#1987_알파벳.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+sys.stdin = open('../a.txt', 'r')
+
+def dfs(i, k, cnt):
+    global ans
+    ans = max(cnt, ans)
+
+    for j in range(4):
+        ni = i + di[j]
+        nk = k + dk[j]
+
+        if 0<= ni <R and 0<= nk <C and V[ord(lst[ni][nk]) - 65] == 0:
+            V[ord(lst[ni][nk]) - 65] = 1
+            dfs(ni, nk, cnt + 1)
+            V[ord(lst[ni][nk]) - 65] = 0
+
+R, C = map(int, input().split())
+lst = [list(input()) for _ in range(R)]
+
+di = [-1, 0, 1, 0]
+dk = [0, 1, 0, -1]
+
+V = [0] * 26
+V[ord(lst[0][0]) - 65] = 1
+
+ans = 0
+dfs(0, 0, 0)
+print(ans + 1)
---
+++ BOJ###.py
@@ -0,0 +1,16 @@
+#2810
+import sys
+sys.stdin = open('a.txt', 'r')
+
+N = int(input())
+lst = list(input())
+
+cnt = 2
+for i in range(N-1):
+    pass
+if cnt > N:
+    print(N)
+else:
+    print(cnt)
+
+
