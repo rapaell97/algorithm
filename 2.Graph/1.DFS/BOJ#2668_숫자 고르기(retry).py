96f0c59fdbef94fba2aaf6ed0885aedeaa75e778
minhyeok <rapaell97@naver.com>
2024-04-01 19:36:00 +0900

    240401

---
+++ 2.Graph/1.DFS/BOJ#2668_숫자 고르기(retry).py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open('../../a.txt', 'r')
+def dfs(i, t):
+    V[i] = 1
+
+    if V[lst[i]] == 0:
+        dfs(lst[i], t)
+    elif V[lst[i]] == 1 and lst[i] == t:
+        arr.append(lst[i])
+
+
+N = int(input())
+lst = [0] + [int(input()) for _ in range(N)]
+
+arr = []
+for i in range(1, N + 1):
+    V = [0] * (N + 1)
+    dfs(i, i)
+
+arr.sort()
+print(len(arr))
+for i in arr:
+    print(i)
